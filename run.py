e2eb60c82624d3de9c8be29a3056e8785d8c5287
terekli <35097082+terekli@users.noreply.github.com>
2024-03-18 21:40:31 -0400

    Update run.py

--- run.py
+++ run.py
@@ -5,11 +5,9 @@ from generate_gcode import save_gcode
 layer_thickness = 0.5 # mm
 line_width = 1 # mm
 gantry_speed = 40 # mm/min
-# stl_path = '/Users/terekli/Desktop/slicer2/stl/cube_with_mesh.stl'
-stl_path = '/Users/terekli/Desktop/BotSlicer/stl/grid.stl'
-# stl_path = '/Users/terekli/Desktop/slicer2/stl/3DBenchy.stl'
-# stl_path = '/Users/terekli/Desktop/slicer2/stl/logo.stl'
-# stl_path = '/Users/terekli/Desktop/slicer2/stl/capacitor.stl'
+
+stl_path = ''
+
 
 output = BotSlice(stl_path, layer_thickness, line_width, gantry_speed)
 
